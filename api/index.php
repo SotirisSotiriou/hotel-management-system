f8240034e8e5cd2044f1980f749fadd25a9fd942
Sotiris Sotiriou <sotiriou.sot@yahoo.com>
2022-11-06 17:34:39 +0200

    add some code

---
+++ api/index.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+require dirname(__DIR__) . "/vendor/autoload.php";
+
+set_error_handler("ErrorHandler::handleError");
+set_exception_handler("ErrorHandler::handleException");
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+header("Content-type: application/json; charset=UTF-8");
+
+var_dump($_POST);
